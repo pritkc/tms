eb431e069ffe5de8711faf733f9925f07a8f7c67
pritkc <74728118+pritkc@users.noreply.github.com>
2022-06-09 12:10:05 +0530

    first commit

---
+++ admin/includes/footer.php
@@ -0,0 +1,3 @@
+<div class="copyrights">
+	 <p>© 2022 Privilege. All Rights Reserved |  <a href="#">Privilege</a> </p>
+</div>	
---
+++ pg.php
@@ -0,0 +1,28 @@
+<?php
+
+$apikey = "rzp_test_nKSIozGBHfEBX8";
+
+?>
+
+<div class="row" style="padding:100px 300px;">
+<div class= "col-50">
+<div class= "container">
+
+<form action="payscript.php" method="post" style="padding : 25px;">
+<div class = "row">
+    <div class = "col-25">
+
+    <h3>Checkout form </h3>
+    <label for="fname"> full name</label>
+    <input type="text" id="fname" name="name" placeholder="enter legal name here"> 
+
+    <label for="amount"> Amount</label>
+    <input type="number" id="amount" name="amount" placeholder="enter amount in rs. here"> 
+           </div> 
+
+           <input type="submit" value="pay now" class="btn">
+</form>
+</div>
+</div>
+</div>
+</div>
